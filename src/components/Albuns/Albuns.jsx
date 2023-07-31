1976fdb6cdc36ed0629a83495a2ebb21d58195a9
Matheus Monteiro <matheus_wellyson2015@hotmail.com>
2023-07-31 11:43:34 -0300

    feat: instalação do react-icons e estilização do componente album

--- src/components/Albuns/Albuns.jsx
+++ src/components/Albuns/Albuns.jsx
@@ -1,10 +1,36 @@
 import './Albuns.css'
+import { TbPlaylistAdd } from 'react-icons/tb';
+import { MdDelete } from 'react-icons/md';
+import {useState} from 'react';
 
-const Albuns = () => {
+const Albuns = ({data}) => {
+	
+
 	return (
-		<div>
-			<div className="albumTitle">Album: Rei do Gado, 1961</div>
-		</div>
+	
+		<div className="album">
+			<div className="albumTitle">
+			{data.name}
+			<TbPlaylistAdd className="addTrackIcon"/>
+			<MdDelete className="deleteTrackIcon"/>
+			</div>
+			
+			<div className="col-1">
+				<div>Nº</div>
+				<div>Faixa</div>
+				<div>Duração</div>
+			</div>
+			
+			{data.tracks.map(data => (
+				<div className="col-2">
+					<div className="trackNumber">{data.number}</div>
+					<div className="trackName">{data.title}</div>
+					<div className="trackDelete" onClick={() => window.confirm("Você realmente deseja apagar essa música?")}><MdDelete/></div>
+					<div className="trackDuration">{data.duration}</div>
+				</div>	
+			))}
+				
+		</div>	
 	)
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,61 @@
 import logo from '../../assets/logo.png';
+import {useState} from 'react';
 import Albuns from '../../components/Albuns/Albuns.jsx'
+import { IoMdAddCircle } from 'react-icons/io';
 import './Home.css';
 
 const Home = () => {
+	const [albumHover, setAlbumHover] = useState(false);
+
+	const data = [
+		{
+			"name": "Rei do Gado",
+			"year": "1991",
+			"tracks": [
+				{
+				"number": "1",
+				"title": "Borboleta",
+				"duration": "36"
+				},
+				{
+				"number": "2",
+				"title": "Abelha",
+				"duration": "42"
+				},
+				{
+				"number": "3",
+				"title": "Aranha",
+				"duration": "23"
+				},
+
+			]
+		},
+		{
+			"name": "Rei do Gado",
+			"year": "1991",
+			"tracks": [
+				{
+				"number": "1",
+				"title": "Borboleta",
+				"duration": "36"
+				},
+				{
+				"number": "2",
+				"title": "Abelha",
+				"duration": "42"
+				},
+				{
+				"number": "3",
+				"title": "Aranha",
+				"duration": "23"
+				},
+
+			]
+		},
+		
+
+	]
+
 	return (
 	<>
 		<div className="container">
@@ -12,16 +65,20 @@ const Home = () => {
 				<div className="title">Discografia</div>
 			</div>
 			<div className="albumsSection">
-				<div className="AlbumSearch">
+				<div className="albumSearch">
 					<div className="searchText">Digite uma palavra chave</div>
 					<div className="inputArea">
 						<input type="text"/>
 						<button>Procurar</button>
 					</div>
 				</div>
-				<div className="Albums">
-					<Albuns/>
-				</div>	
+				<div className="addPlaylistButton">
+					<IoMdAddCircle />
+				</div>
+				{data.map(data => (
+					<Albuns data={data}/>
+				))}
+				
 			</div>
 		</div>	
 		</div>	
